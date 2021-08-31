8b6267a1732700a420e8948c1862c9771c7a9417
niles <niles@LAPTOP-3K24CQ1R>
2021-09-01 00:27:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleAppPredictOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] str = new String[] {
+
+                "Sunflower","Rose","Jasmine","Marigld","Lily","Lotus"
+            };
+
+            // copying an array
+
+            String[] Info = str;
+
+            foreach (string k in Info)
+
+            {
+
+                Console.WriteLine(k[2]);        // student output :- Jasmine
+
+            }
+        }
+    }
+}
